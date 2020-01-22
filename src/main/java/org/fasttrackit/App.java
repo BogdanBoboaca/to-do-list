51a0548bd571c8eeb14eb7b43b4dc8a96a4cb1a1
BogdanBoboaca <bogdan.m.boboaca@gmail.com>
2020-01-22 19:57:54 +0200

    Added CRUD methods in task repository

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -2,15 +2,12 @@ package org.fasttrackit;
 
 import org.fasttrackit.persistance.TaskRepository;
 import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws IOException, SQLException {
@@ -21,5 +18,12 @@ public class App
         request.setDeadline(LocalDate.now().plusWeeks(1));
 
         taskRepository.createTask(request);
+
+//        UpdateTaskRequest request = new UpdateTaskRequest();
+//        request.setDone(true);
+//
+//        taskRepository.updateTask(1,request);
+
+//        taskRepository.deleteTask(2);
     }
 }
